3f660ec6640131b8dca206d1b7ce3f093d010b5b
Michael <michael.flater@gmail.com>
2024-12-15 19:46:15 -0800

    Update sport-score.js

--- sport-score.js
+++ sport-score.js
@@ -4,6 +4,57 @@
 
 //@ts-check
 
+
+
+
+/ Get Teams:
+// basketball/nba/teams
+// baseball/mlb/teams
+// football/nfl/teams
+// football/ncaa/teams
+// https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams
+// ESPN API endpoint for team data
+// https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/SF
+
+// let scores = await getScores('football', 'nfl');
+// let table = new UITable();
+// table.showSeparators = true;
+// populateTable(table, scores);
+// QuickLook.present(table);
+
+const baseUrl = "https://site.api.espn.com/apis/site/v2/sports/";
+async function getTeams(sport, league, team) {
+  // Query url
+  const url = baseUrl + sport + '/' + league + '/teams/' + team;
+
+  // Initialize new request
+  const request = new Request(url);
+
+  // Execute the request and parse the response as json
+  const response = await request.loadJSON();
+
+  // Return the returned launch data
+  return response;
+}
+
+const baseScoreUrl = "http://site.api.espn.com/apis/site/v2/sports/"
+async function getScores(sport, league) {
+  // Query url
+  const url = baseScoreUrl + sport + '/' + league + '/scoreboard';
+
+  // Initialize new request
+  const request = new Request(url);
+
+  // Execute the request and parse the response as json
+  const response = await request.loadJSON();
+
+  // Return the returned launch data
+  return response;
+}
+
+
+
+
 /**
  * Create the widget
  * @param {{widgetParameter: string, debug: string}} config widget configuration
