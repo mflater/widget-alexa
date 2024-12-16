4cc72fa45a517bee667b316539d93e7d64c31cfa
Michael <michael.flater@gmail.com>
2024-12-15 17:36:40 -0800

    Update widget-alexa.js

--- widget-alexa.js
+++ widget-alexa.js
@@ -7,11 +7,11 @@
 // ESPN API endpoint for team data
 // https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/SF
 
-  let scores = await getScores('football', 'nfl');
-  let table = new UITable();
-  table.showSeparators = true;
-  populateTable(table, scores);
-  QuickLook.present(table);
+let scores = await getScores('football', 'nfl');
+let table = new UITable();
+table.showSeparators = true;
+populateTable(table, scores);
+QuickLook.present(table);
 
 const baseUrl = "https://site.api.espn.com/apis/site/v2/sports/";
 async function getTeams(sport, league, team) {
