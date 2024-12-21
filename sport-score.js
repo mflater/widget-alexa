193184b3486b8f00a1cfa3bb5a0eebec640f7ad4
Michael <michael.flater@gmail.com>
2024-12-21 10:54:45 -0800

    Update sport-score.js

--- sport-score.js
+++ sport-score.js
@@ -69,11 +69,17 @@ async function getTeamLogo(url) {
 // }
 
 async function addCompetition (main, teamData, info) {
-  let competition = teamData.team.nextEvent[0].competitions[0];
-  let gameStatus = competition.status.type.shortDetail;
-  let gameState = competition.status.type.state;
-  let homeTeamPath = competition.competitors[0];
-  let awayTeamPath = competition.competitors[1];
+  // let competition = teamData.team.nextEvent[0].competitions[0];
+  let gameStatus = teamData.team.nextEvent[0].competitions[0].status.type.shortDetail;
+  let gameState = teamData.team.nextEvent[0].competitions[0].status.type.state;
+  let homeTeamPath = teamData.team.nextEvent[0].competitions[0].competitors[0];
+  let awayTeamPath = teamData.team.nextEvent[0].competitions[0].competitors[1];
+
+  // let competition = teamData.team.nextEvent[0].competitions[0];
+  // let gameStatus = competition.status.type.shortDetail;
+  // let gameState = competition.status.type.state;
+  // let homeTeamPath = competition.competitors[0];
+  // let awayTeamPath = competition.competitors[1];
   
   // let htCode = homeTeamPath.team.abbreviation;
   let htImage = await getTeamLogo(homeTeamPath.team.logos[0].href);
